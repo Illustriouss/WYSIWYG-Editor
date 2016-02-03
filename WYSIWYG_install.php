af6c7a9d734b5cd1ceaefcf8bb00fdfc32129e13
Dacaspex <Caspersmits11@outlook.com>
2016-02-03 12:56:49 +0100

    Created start for installation page
    Created php page for installation and the corresponding CSS

---
+++ WYSIWYG_install.php
@@ -0,0 +1,32 @@
+<?php
+
+	
+
+?>
+<html>
+	<head>
+		<title>WYSIWYG installation</title>
+		<link rel="stylesheet" type="text/css" href="css/install.css">
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,300,100' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="wrapper">
+			<h1>Installation</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h1>Editor options</h1>
+			<p>Here you can specify all the options for the editor.</p>
+			<h2>Button options</h2>
+			<div class="option-group">
+				<div class="option"><label>Basic text operation optinos</label><input type="checkbox" /></div>
+			</div>
+			<h2>Other options</h2>
+			<h1>Database settings</h1>
+			<h1>Import style</h1>
+		</div>
+	</body>
+</html>
